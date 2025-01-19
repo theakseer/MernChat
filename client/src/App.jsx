ebc99a699fc1714a20fb88b83e97aa3a3c143c1f
[Akseer Ahmad] <68474681+theakseer@users.noreply.github.com>
2025-01-19 19:03:59 +0530

    v1.3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { useAuthContext } from './context/AuthContext'
 
 function App() {
   const {authUser} = useAuthContext()
-  return (
+  return ( 
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
         <Route path='/' element={authUser==null ? <Navigate to='/login'/> : <Home/>} />
